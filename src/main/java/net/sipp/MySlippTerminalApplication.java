b48520a105f03324eabc21c50a248843acb62925
braverokmc79 <braverokmc79@gmail.com>
2017-08-03 19:06:51 +0900

    first commit

---
+++ src/main/java/net/sipp/MySlippTerminalApplication.java
@@ -0,0 +1,12 @@
+package net.sipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySlippTerminalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySlippTerminalApplication.class, args);
+	}
+}
